e00b0ac576b54ad983137a61b75884e4339359a4
husseinmaad <hussein.maad@outlook.com>
2016-12-06 21:04:12 -0800

    change the input data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,14 @@
 puts "whats your name"
-h_name = gets.chomp
+h_name = gets.chomp.to_s
 puts "what volume level do you like that helps you to sleep?"
-volume = gets.chomp 
+volume = gets.chomp.to_i
 puts "whats your favorite fur color?"
-color = gets.chomp
-puts " Are you a good candidate for adoption?"
-adoption = gets.chomp 
+color = gets.chomp.to_s
+puts "Are you a good candidate for adoption (y/n)?"
+adoption = gets.chomp.to_s
 puts "what is your estimated age?"
-age = gets.chomp 
-if age.empty == true
+age = gets.chomp.to_i
+if age == 0
     age = nil
 end
+
