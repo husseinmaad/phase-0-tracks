63ba2cea577e90413faaceeeadf9dc967b3967bc
husseinmaad <hussein.maad@outlook.com>
2016-12-07 23:22:46 -0800

    add some qeustions and input way to answer them

---
+++ ruby/vampires.rb
@@ -0,0 +1,7 @@
+puts "Whats your name?"
+emp_name = gets.chomp.to_s
+puts "How old are you?"
+emp_age = gets.chomp.to_i
+puts "What year were you born"
+emp_born_year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
