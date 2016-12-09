38a22fc0dfbd2c09afdb9eb9f17acaf7a51f82ed
husseinmaad <hussein.maad@outlook.com>
2016-12-08 19:24:51 -0800

    Add the pseudocode for the  methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,15 @@
+=begin
+  - get the input
+  - get the length 
+  - until the index = length -1 
+  - on each one get the index number 
+    - change that latter to the next latter.
+=end
+=begin
+  - store the alphabet in a var
+  - utile the the index length -1
+  - match the latter in the latter in the alphabet
+  - get the index value in the latter in alphabet
+  - return the latter that is 1 less then the index value
+  - store it in our var
+=end
