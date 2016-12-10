11a131698253dd9990318e142c7cc8a20ada5aff
husseinmaad <hussein.maad@outlook.com>
2016-12-09 17:58:24 -0800

    add loop to process more than one employee

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,39 +1,49 @@
-# this method to convert the string to boolean
-def boolean(string)
-  if string.downcase == "y"
-    return true
-  else 
-    return false 
+
+puts "How many employees you will be process?"
+num_emp_process = gets.chomp.to_i
+
+while num_emp_process > 0 do
+  
+  # this method to convert the string to boolean
+  def boolean(string)
+    if string.downcase == "y"
+      return true
+    else 
+      return false 
+    end 
+  end
+
+  # verify the age method 
+  def age_verify(age, year)
+    return (Time.new.year - year == age ? true : false)
   end 
-end
-# verify the age method 
-def age_verify(age, year)
-  return (Time.new.year - year == age ? true : false)
-end 
 
-puts "Whats your name?"
-emp_name = gets.chomp.to_s
-puts "How old are you?"
-emp_age = gets.chomp.to_i
-puts "What year were you born"
-emp_born_year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
-eat_garlic = boolean(gets.chomp)
-puts "Would you like to enroll in the company’s health insurance (y/n)?"
-health_ins = boolean(gets.chomp)
+  puts "Whats your name?"
+  emp_name = gets.chomp.to_s
+  puts "How old are you?"
+  emp_age = gets.chomp.to_i
+  puts "What year were you born"
+  emp_born_year = gets.chomp.to_i
+  puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+  eat_garlic = boolean(gets.chomp)
+  puts "Would you like to enroll in the company’s health insurance (y/n)?"
+  health_ins = boolean(gets.chomp)
 
-age_after_verify = age_verify(emp_age, emp_born_year)
+  age_after_verify = age_verify(emp_age, emp_born_year)
 
-check = "Probably not a vampire." if age_after_verify && (eat_garlic||health_ins)
-      
-check = "Probably a vampire." if !age_after_verify && (eat_garlic||health_ins) 
+  check = "Probably not a vampire." if age_after_verify && (eat_garlic||health_ins)
+        
+  check = "Probably a vampire." if !age_after_verify && (eat_garlic||health_ins) 
+         
+  check = "Almost certainly a vampire." if !age_after_verify && !eat_garlic && !health_ins 
        
-check = "Almost certainly a vampire." if !age_after_verify && !eat_garlic && !health_ins 
-     
-check = "Definitely a vampire." if emp_name == ("Drake Cula" || "Tu Fang")
- 
-check = "Results inconclusive" if check == nil
+  check = "Definitely a vampire." if emp_name == ("Drake Cula" || "Tu Fang")
+   
+  check = "Results inconclusive" if check == nil
 
-print check
-     
+  print check
+  
+
+  num_emp_process -= 1
+end   
 
