e5516f5c96e73039b0a42e69a2a67f150e3ac1d6
husseinmaad <hussein.maad@outlook.com>
2016-12-09 22:09:25 -0800

    add boolean method

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,3 +1,11 @@
+def boolean(string)
+  if string.downcase == "y"
+    return true
+  else
+    return false
+  end
+end 
+
 puts "whats your name"
 h_name = gets.chomp.to_s
 puts "what volume level do you like that helps you to sleep?"
@@ -5,7 +13,7 @@ volume = gets.chomp.to_i
 puts "whats your favorite fur color?"
 color = gets.chomp.to_s
 puts "Are you a good candidate for adoption (y/n)?"
-adoption = gets.chomp.to_s
+adoption = boolean(gets.chomp)
 puts "what is your estimated age?"
 age = gets.chomp.to_i
 if age == 0
