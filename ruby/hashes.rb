68d29e18c3e393fb0d3bfed59eb34c5edc273b21
husseinmaad <hussein.maad@outlook.com>
2016-12-13 17:10:58 -0800

    add the pseudocode for the challenge

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+=begin
+  - create an empty hash with valid keys.
+  thats covers the input values. 
+  - add user input interface. 
+  - Convert any user input to 
+  the appropriate data type.
+  - assign the input value to the correct key name.
+  - print the user inputs to help the designer review the questions answers.
+  - prompt message to ask user if he likes to update some values
+  - if the user say "none":
+    - skip this part and print the hash info. again
+    otherwise: ask the user which value he would like to edit
+          - convert the user input to hash key .
+          - ask the user to input the new value. 
+          - update the key value
+          - reprint the latest update.
+= end
+  
