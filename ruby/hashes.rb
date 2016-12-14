4cc2bf9aef48de2b5f9f22aae243d6cc734d1072
husseinmaad <hussein.maad@outlook.com>
2016-12-14 02:40:36 -0800

    add the UI and set the inputs

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,11 +7,32 @@
   - prompt message to ask user if he likes to update some values
   - if the user say "none":
     - skip this part and print the hash info. again
-    otherwise: ask the user which value he would like to edit
+    otherwise: 
           - convert the user input to hash key .
           - ask the user to input the new value. 
           - update the key value
           - reprint the latest update.
-= end
+=end
   
+client_details = {}
+puts "Welcome To The Client Details Form"
+puts "Please answer the fallowing questions"
 
+puts "Enter the client's name"
+client_details[:name] = gets.chomp.to_s
+
+puts "Age"
+client_details[:age] = gets.chomp.to_i 
+
+puts "Sex"
+client_details[:sex] = gets.chomp.to_s
+
+puts "Children number"
+client_details[:children_number] = gets.chomp.to_i 
+
+puts "decor theme"
+client_details[:decor_theme] = gets.chomp.to_s
+
+puts "allergist's color"
+client_details[:allergist_color] = gets.chomp.to_s
+ 
