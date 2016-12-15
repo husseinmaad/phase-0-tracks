148d2984a80dba861fb6bf1aea32d4ae979208fc
husseinmaad <hussein.maad@outlook.com>
2016-12-14 18:44:38 -0800

    add github repo nested data structuer

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,29 @@
+github = {
+    first_account:{
+      name:'Hussein',
+      repositors: {
+        phase0_track:{
+          content_list: ['Ruby','CSS','HTML','Git','JS']
+        },
+        personal:{
+          content_list: ['examples','test']
+        },
+        website: {
+          content_list: ['index','images','logo','pages']
+        }
+      }, 
+    },
+      second_account: {
+        name: 'Ana',
+        repositors:{
+          personal: {
+            content_list:['exams','homeworks','cheat sheet']
+          },
+          programming: {
+            content_list:['c++','HTML','C#','python','VBA']
+          },
+        },
+  
+    },
+}
+p github
