96a76b2f4260e01b7b031ffaa850cc460299f53b
husseinmaad <hussein.maad@outlook.com>
2016-12-14 19:28:19 -0800

    demonstrate a few different types of access to the nested data structuer

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -26,4 +26,32 @@ github = {
   
     },
 }
-p github
+
+
+p github[:first_account][:name]
+
+p github[:second_account][:name]
+
+p github[:first_account][:repositors][:phase0_track][:content_list].sort
+
+p github[:first_account][:repositors][:personal][:content_list].delete_if{|item| item == 'test'}
+
+p github[:second_account][:repositors][:programming][:content_list].reverse
+
+p github [:first_account][:name].chars
+
+p github [:second_account][:name].upcase
+
+p github [:second_account][:repositors][:personal][:content_list].map!{|file| file.upcase}
+
+p github [:second_account][:repositors][:personal].clear
+
+p github [:second_account][:repositors][:programming].empty?
+
+p github [:first_account][:repositors][:website][:content_list].include?("index")
+p github[:second_account][:repositors][:programming][:content_list].length
+
+puts "Website Folder Contain"
+github [:first_account][:repositors][:website][:content_list].each do |item|
+  puts "#{item}"
+end
