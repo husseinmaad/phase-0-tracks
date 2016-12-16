1337bf6ac5c9192945557c3117b456db94616665
husseinmaad <hussein.maad@outlook.com>
2016-12-15 21:57:25 -0800

    pseudocode the first method that hundls vowels letters

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+=begin
+ - Create methods to check the vowel letter and return the new one if found
+
+    - if that latter is exist in the vowel array by using the include method
+        - if its equal true means you find the letter 
+            -check if the letter equal to "u" then the letter equal "a" to avoid the nil value
+            otherwise:
+            - use the built in method index to get the index and increment by 1
+            - assign the value to the letter and return the new letter value from the vowel array
+            - stop  
+    -otherwise :
+       
+=end
