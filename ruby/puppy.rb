f3b1faacccf215ccf89acefe303b1abd69bd4d94
husseinmaad <hussein.maad@outlook.com>
2016-12-19 22:50:00 -0800

    defin a puppy class in comment

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+=begin
+Dogs ------------------------
+Puppies
+
+CHARACTERISTICS ----------------
+Puppies length: varies
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Walk
+Park
+Bite
+=end
