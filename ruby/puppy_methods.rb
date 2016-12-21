a68752893b3e9eb69c821f2550b7b173cfa1a2a8
husseinmaad <hussein.maad@outlook.com>
2016-12-20 17:32:25 -0800

    add methods to the puppy class with driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,32 @@
 class Puppy
+    def initialize
+      puts  "Initializing new puppy instance ..."
+    end 
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+    def fetch(toy)
+        puts "I brought back the #{toy}!"
+        toy
+    end
 
+    def speak(number)
+        number.times{|x| puts "Woof!"}
+    end 
+    def roll_over
+        print "\"*rolls over*\"\n"
+    end 
+    def dog_year(h_age)
+        dog_age = h_age * 7
+        return dog_age
+    end 
+
+    def have_five
+      puts "Have Five !!"
+    end 
 end
 
+max = Puppy.new
+max.fetch("ball")
+max.speak(4)
+max.roll_over
+p max.dog_year(2)
+max.have_five
