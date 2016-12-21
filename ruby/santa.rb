f3213b0796333bd8848b50997830c13f017c78fd
husseinmaad <hussein.maad@outlook.com>
2016-12-21 13:13:36 -0800

    update instance_behave method and format the code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,6 +17,9 @@ class Santa
     def eat_milk_and_cookies(cookie_type)
         puts "That was a good #{cookie_type}"
     end
+    def print_info
+        puts "your gender is #{@gender} and your ethnicity is #{@ethnicity}"
+    end 
 end 
 
 # Test the Santa class
@@ -25,25 +28,32 @@ end
 #hussein.speak
 #hussein.eat_milk_and_cookies("chocolate")
 
+
+# Driver code methods
+
 def create_instances(gen_list,ethn_list)
     arr_instances = []
     len = gen_list.length
-    len.times do |idx|
-        arr_instances << Santa.new(gen_list[idx],ethn_list[idx])
-    end 
+    len.times {|idx|arr_instances << Santa.new(gen_list[idx],ethn_list[idx])}
     return arr_instances
 end 
-
-gender_list = ['male','N/A','female','male','female','male']
-ethnicity_list= ["white", "Latino", "white","black","N/A","Latino"]
-
-new_instances = create_instances(gender_list,ethnicity_list)
 
 def instance_behave(list)
     list.each do |instance|
         puts " #{instance}"
         instance.speak
+        puts "---------------------"
         instance.eat_milk_and_cookies("chocolate")
+        puts "---------------------"
+        instance.print_info
+        puts "---------------------"
     end 
 end 
+
+# Test driver code
+
+gender_list = ['male','N/A','female','male','female','male']
+ethnicity_list= ["white", "Latino", "white","black","N/A","Latino"]
+
+new_instances = create_instances(gender_list,ethnicity_list)
 instance_behave(new_instances)
