70355a21c9452d02ce75c1c31c2eac9e540188e4
husseinmaad <hussein.maad@outlook.com>
2016-12-20 19:54:52 -0800

    add new class and add 3 methods inside the class and add 2 methods to work with the class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -30,3 +30,38 @@ max.speak(4)
 max.roll_over
 p max.dog_year(2)
 max.have_five
+
+# Cerate soccer class
+
+class Soccer_Players
+def initialize
+ puts  "Initializing new soccer player instance ..."
+end
+def kick_the_ball
+ puts "Kick the ball!!!"
+end
+def players_speed(speed)
+ puts "the player speed is #{speed}"
+end
+end
+
+def create_instance(number)
+players = {}
+idx = 0 
+   while idx < number+1
+      players["player#{idx}"] = Soccer_Players.new
+      idx +=1 
+   end
+   return players
+end
+new_player = create_instance(50)
+p new_player
+
+def iterate_instance(list)
+   list.each do |player,value|
+     list[player].kick_the_ball
+     list[player].players_speed(90)
+  end
+end
+
+iterate_instance(new_player)
