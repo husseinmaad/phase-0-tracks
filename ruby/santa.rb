4523c23981a25ae03b2f11405a3b9fed9c8e8b9f
husseinmaad <hussein.maad@outlook.com>
2016-12-20 21:19:44 -0800

    update the initialize method and add some attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,14 @@
 # Create Santa class
 
 class Santa
+    def initialize(gender,ethnicity)
+        @gender = gender
+        @ethnicity = ethnicity
+        @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet",
+         "Cupid", "Donner", "Blitzen"]
+        @age = 0
+        puts "Initializing Santa instance ..." 
+    end 
 
     def speak
         puts "Ho, ho, ho! Haaaappy holidays!"
@@ -9,9 +17,6 @@ class Santa
     def eat_milk_and_cookies(cookie_type)
         puts "That was a good #{cookie_type}"
     end
-    def initialize
-        puts "Initializing Santa instance ..." 
-    end 
 end 
 
 # Test the Santa class
