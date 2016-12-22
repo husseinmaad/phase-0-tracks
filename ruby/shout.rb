e971a1072b9d5f06b017dd4de915c4235084c56a
husseinmaad <hussein.maad@outlook.com>
2016-12-22 01:33:19 -0800

    redo the module class and defin to classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
   def self.yell_angrily(words)
@@ -7,3 +8,21 @@ module Shout
     words + "!" + " :)"
   end 
 end
+=end
+module Shout
+   def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+ def yelling_happily(words)
+    words + "!" + " :)"
+  end 
+end 
+
+class Scary
+  include Shout
+end 
+
+class Funny 
+  include Shout
+end 
+
