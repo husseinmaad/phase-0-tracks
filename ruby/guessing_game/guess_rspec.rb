a4bd564638fee523ef33a23d0b9d30e3f2c02e26
husseinmaad <hussein.maad@outlook.com>
2016-12-23 20:19:16 -0800

    add code to teset the process method with one letter

--- ruby/guessing_game/guess_rspec.rb
+++ ruby/guessing_game/guess_rspec.rb
@@ -6,7 +6,10 @@ describe Guessing do
   it "get word method" do 
     expect(guess1.get_word("hi")).to eq ["h","i"]
   end  
-  it "check the process method that should print the guess letter" do
-    expect().to eq 
+  it "check the process method that should print the guess for one letter" do
+    guess1.arr_w = ['w','f','e','w','r']
+    guess1.arr_g = ['w']
+    expect(guess1.process).to eq false 
   end  
+ 
 end
