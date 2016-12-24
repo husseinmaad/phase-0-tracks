c7a6b21b087d8fd38eee616acae69d2c0743e340
husseinmaad <hussein.maad@outlook.com>
2016-12-23 21:16:48 -0800

    add the user interface and add loop

--- ruby/guessing_game/guess.rb
+++ ruby/guessing_game/guess.rb
@@ -7,13 +7,15 @@
 # crate method to do the process 
   # on each char in the word array 
   # check if the letter in side the word array 
+  # print the char if its inside the guess array
+  #otherwise print "-"
 
 #crate loop that not stop if the user didn't guess the letter and it will stop 
 #as soon as the user figured out the hole word.
   # set the loop equal to the limit
 
   class Guessing
-      attr_accessor :arr_w, :arr_g
+      attr_accessor :arr_w, :arr_g, :limit
       def initialize
          @arr_w = []
          @arr_g = []
@@ -39,4 +41,28 @@
             puts "\n"
          return @is_over
       end
+
+  end 
+
+
+
+  idx =0
+  guess_word = Guessing.new
+  puts "Please enter the word the you would like th next person to guess"
+  guess_word.get_word(gets.chomp)
+
+  while idx < limit + 1 do
+      puts "please enter your guessing letter"
+      letter = gets.chomp
+      if guess_word.arr_w.include?(letter)
+         guess_word.arr_g << letter
+         if guess_word.process == false
+            puts "Yahh! your guess letter is right :)\n"
+            idx +=1
+         else 
+            break
+         end 
+      else 
+         puts "Sorry! your guess letter is wrong :("
+      end 
   end 
