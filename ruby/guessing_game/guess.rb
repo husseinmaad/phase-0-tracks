fc57b55c0dd08881f9bfe6e78d6513813bb378f2
husseinmaad <hussein.maad@outlook.com>
2016-12-23 17:49:08 -0800

    add psudocode for the work plan

---
+++ ruby/guessing_game/guess.rb
@@ -0,0 +1,12 @@
+# create class for the guessing game 
+# add initialize method and has some attr. like word, guess letter, limit
+# add get word method
+  # get the word from the user
+  # set the the word equal to attr word
+  # set the attr. limit equal to the word length
+# crate method to do the process 
+  #
+
+#crate loop that not stop if the user didn't guess the letter and it will stop 
+#as soon as the user figured out the hole word.
+  # set the loop equal to the limit
