13c9d2070cfcc117a41af29892d2f35f901dac73
husseinmaad <hussein.maad@outlook.com>
2017-01-04 17:13:05 -0800

    add code to loop throught array abd assgine pair of value '

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,11 @@ var names = ["Bill", "Ed", "Mack", "Ned"]
 
 colors.push("pink");
 names.push("Chad");
+
+ var horse = {} ;
+
+ for (var k=0; k < names.length; k++)
+ {
+   horse[names[k]] = colors[k];
+ }
+ console.log(horse)
